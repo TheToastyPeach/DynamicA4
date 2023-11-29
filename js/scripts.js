ec78f7b61ecc49b32ca32d3db540c52ef15d8e26
Hans Bachman <hans.lovestanks@gmail.com>
2023-11-29 11:58:21 -0700

    Changing extending methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,50 @@
 
 
 function extendPageHeight() {
-    let height = 2000; // initial height in pixels
-    const increasePerSecond = 1; // increase in pixels per second
+    let height = 100;
+    // how many pixels will be added after each delay
+    const increasePix = 1; 
 
     const increaseHeight = () => {
-        height += increasePerSecond;
+        height += increasePix;
         document.getElementById('length').style.height = height + "px";
-        setTimeout(increaseHeight, 100);
+        // settimout to delay the recursion
+        setTimeout(increaseHeight, 50); 
     };
 
+    //calling the increase height function again so that it loops infinetly 
     increaseHeight(); 
 }
 
-extendPageHeight();
+
+
+// document.addEventListener("visibilitychange", function() {
+//     if (document.hidden) {
+//         console.log("The tab is not active, extending");
+//         extendPageHeight();
+//     } else {
+//         console.log("The tab is active");
+//     }
+// });
+
+let start;
+var totalUnfocusedTime = 0;
+
+//checks when a tab is not the focus anymore
+document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+        //starts a timer with "date.now()" 
+        start = Date.now();
+    } else {
+        if (start) {
+            //calulates the total time in what I assume are milliseconds
+            totalUnfocusedTime += Date.now() - start;
+            //resets the start value
+            start = null;
+            document.getElementById('length').style.height = ((totalUnfocusedTime + document.getElementById('length').offsetHeight) / 10) + "px";
+            console.log(totalUnfocusedTime);
+            extendPageHeight();
+        }
+    }
+});
+
