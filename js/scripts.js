6a9d0e5a89abd818fe4ad760934dddd2095b68d6
Hans Bachman <hans.lovestanks@gmail.com>
2023-11-29 19:36:38 -0700

    Got the scrolling to work how I wanted it to!

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 
 
-function extendPageHeight() {
-    let height = 100;
-    // how many pixels will be added after each delay
-    const increasePix = 1; 
+// function extendPageHeight() {
+//     let height = 100;
+//     // how many pixels will be added after each delay
+//     const increasePix = 1; 
 
-    const increaseHeight = () => {
-        height += increasePix;
-        document.getElementById('length').style.height = height + "px";
-        // settimout to delay the recursion
-        setTimeout(increaseHeight, 50); 
-    };
+//     const increaseHeight = () => {
+//         height += increasePix;
+//         document.getElementById('length').style.height = height + "px";
+//         // settimout to delay the recursion
+//         setTimeout(increaseHeight, 50); 
+//     };
 
-    //calling the increase height function again so that it loops infinetly 
-    increaseHeight(); 
-}
+//     //calling the increase height function again so that it loops infinetly 
+//     increaseHeight(); 
+// }
 
 
 
@@ -28,21 +28,34 @@ function extendPageHeight() {
 // });
 
 let start;
-var totalUnfocusedTime = 0;
+let totalUnfocusedTime = 0;
+let oldHeight = 0;
 
 //checks when a tab is not the focus anymore
 document.addEventListener("visibilitychange", function() {
     if (document.hidden) {
         //starts a timer with "date.now()" 
         start = Date.now();
+        // oldHeight = document.documentElement.scrollHeight;
     } else {
         if (start) {
+            //ensures that the page is at the top when tabbed back in
+            window.scrollTo(0, 0);
+
             //calulates the total time in what I assume are milliseconds
             totalUnfocusedTime += Date.now() - start;
-            //resets the start value
-            start = null;
-            document.getElementById('length').style.height = ((totalUnfocusedTime + document.getElementById('length').offsetHeight) / 10) + "px";
+
+            //sets the height of the div to the total time in milliseconds divided by 10
+            document.getElementById('length').style.height = ((totalUnfocusedTime + oldHeight) / 10) + "px";
             console.log(totalUnfocusedTime);
+
+            //adds the total time to the old height so the page doesn't reset
+            oldHeight = totalUnfocusedTime + oldHeight;
+            console.log(oldHeight);
+
+            //resets the start values
+            start = null;
+            totalUnfocusedTime = 0;
         }
     }
 });
